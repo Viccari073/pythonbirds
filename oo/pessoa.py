ec0a1ecf6664e04608692386bf33debeb3f8cd2f
Viccari073 <viccari73@gmail.com>
2020-11-04 16:53:28 -0300

    criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -4,21 +4,23 @@
 # OBS: self -> parametro que será um objeto a ser recebido. Não é uma palavra reservada (pode ser qlq coisa).
 
 class Pessoa:  # Classe
-    def __init__(self, nome=None, idade=35):  # Atributo de dado ou atributo de instância.
+    def __init__(self, *filhos, nome=None, idade=35) -> object:  # Atributo de dado ou atributo de instância.
         self.idade = idade
         self.nome = nome  # Está passando para o atributo nome (self.nome) o parâmentro nome (nome=None)
-
+        self.filhos = list(filhos)  # objeto complexo
 
     def cumprimentar(self):  # Método (atributo da classe)
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Ricardo')  # Objeto 'p'
-    print(Pessoa.cumprimentar(p))  # Forma não usual. Declara a classe, chama o método e define o parâmetro.
-    print(id(p))
-    print(p.cumprimentar())  # chamando o método a partir do objeto
-    print(p.nome)
-    p.nome = 'Lucas'
-    print(p.nome)
-    print(p.idade)
+    lucas = Pessoa(nome='Lucas')  # Objeto 'p'
+    ricardo = Pessoa(lucas, nome='Ricardo')
+    print(Pessoa.cumprimentar(ricardo))  # Forma não usual. Declara a classe, chama o método e define o parâmetro.
+    print(id(ricardo))
+    print(ricardo.cumprimentar())  # chamando o método a partir do objeto
+    print(ricardo.nome)
+    print(ricardo.idade)
+    for filhos in ricardo.filhos:
+        print(filhos.nome)
+    print(ricardo.filhos)
