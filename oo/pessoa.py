52ce74aba33c02ab6c6bcbfaaf579d946209335f
Viccari073 <viccari73@gmail.com>
2020-11-09 10:18:41 -0300

    Criado atributo de Classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,8 +3,13 @@
 
 # OBS: self -> parametro que será um objeto a ser recebido. Não é uma palavra reservada (pode ser qlq coisa).
 
+# ATENÇÃO: se o atributo for o mesmo para todos os objetos, o atributo será o de classe, se não, será um atributo de instância.
+
 class Pessoa:  # Classe
-    def __init__(self, *filhos, nome=None, idade=70) -> object:  # Atributo de dado ou atributo de instância.
+
+    olhos = 2  # Atributo de classe ou atributo default.
+
+    def __init__(self, *filhos, nome=None, idade=70):  # Atributo de dado ou atributo de instância.
         self.idade = idade
         self.nome = nome  # Está passando para o atributo nome (self.nome) o parâmentro nome (nome=None)
         self.filhos = list(filhos)  # objeto complexo
@@ -26,7 +31,14 @@ if __name__ == '__main__':
         print(filhos.nome, filhos.idade)
     print(ricardo.filhos)
     del ricardo.filhos  # Removendo um atriburo dinamicamente
+    ricardo.olhos = 1  # altera o atributo do objeto, mas não da classe
+    del ricardo.olhos  # deleta o atributo do objeto, mas não ca classe
     lucas.sobrenome = 'Viccari'  # atributo dinâmico
     print(lucas.__dict__)  # atributo especial. o 'dunder dict' contem os atribuots de instância.
     print(ricardo.__dict__)
+    Pessoa.olhos = 3  # altera o valor do atributo para todos os objetos da classe
+    print(Pessoa.olhos)
+    print(ricardo.olhos)
+    print(lucas.olhos)
+    print(id(Pessoa.olhos), id(ricardo.olhos), id(lucas.olhos))
 
